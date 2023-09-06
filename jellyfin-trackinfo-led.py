0f827717b209d705abebac5a859ae4609def454a
Adrian Salamon <adriansalamon@gmail.com>
2023-09-06 15:20:42 +0200

    fixed crash when jellyfin is not reachable

--- jellyfin-trackinfo-led.py
+++ jellyfin-trackinfo-led.py
@@ -17,41 +17,53 @@ sendheaders = {'Content-Type': 'application/json',
                'x-emby-token': config.jellyfin['apikey'],
                }
 
-# TODO: Build try catch 
-    # if jellyfin is not reachable.
-    # then skip the entire app (instead of crashing)
+def getInfo(devices):
+    currently_playing = []
+    for device in devices.json():
+        # Filter only devices that are currently playing something
+        if device.get('NowPlayingItem'):
+            currently_playing.append(device)
+            #  Handle mutliple users use the service. Track the correct user
+            try:
+                currently_playing[0]['UserName'] == config.jellyfin['user']
+                print("[INFO] user", config.jellyfin['user'], "detected")
+                track = currently_playing[0]['NowPlayingItem']['Name']
+                artist = currently_playing[0]['NowPlayingItem']['Artists'][0]
+                mytext = "Now playing »" + track + "« by " + artist
+
+            # Send track-info to LED-Matrix
+                print("[INFO]", mytext)
+                pixelit.sendApp(
+                    text_msg=mytext,
+                    red=255,
+                    green=255,
+                    blue=255,
+                    icon="[0,0,0,0,0,0,0,0,0,0,65535,65535,65535,65535,65535,0,0,0,65535,0,0,0,65535,0,0,0,65535,0,0,0,65535,0,0,0,65535,0,0,0,65535,0,0,65535,65535,0,0,65535,65535,0,0,65535,65535,0,0,65535,65535,0,0,0,0,0,0,0,0,0]",
+                    bigFont="false",
+                    scrollText="auto",
+                    centerText="false",
+                    )
+                # on first match, leave cycling through devices
+                break
+            except: 
+                print("[DEBUG] No device playing for user",config.jellyfin['user'])
+    if not currently_playing:
+        print("[DEBUG] No device playing.")
+        pixelit.skipApp()
 
 # Check for devices that have checked in in the last 90 seconds
-devices = requests.get(
-    config.jellyfin['url'] +
-    '/Sessions?ActiveWithinSeconds=90',
-    headers=sendheaders)
-currently_playing = []
-
-for device in devices.json():
-    # Filter only devices that are currently playing something
-    if device.get('NowPlayingItem'):
-        currently_playing.append(device)
-        #  Handle mutliple users use the service. Track the correct user
-        if(currently_playing[0]['UserName'] == config.jellyfin['user']):
-            print("[INFO] user", config.jellyfin['user'], "detected")
-            track = currently_playing[0]['NowPlayingItem']['Name']
-            artist = currently_playing[0]['NowPlayingItem']['Artists'][0]
-            mytext = "Now playing »" + track + "« by " + artist
-
-        # Send track-info to LED-Matrix
-            print("[INFO]", mytext)
-            pixelit.sendApp(
-                text_msg=mytext,
-                red=255,
-                green=255,
-                blue=255,
-                icon="[0,0,0,0,0,0,0,0,0,0,65535,65535,65535,65535,65535,0,0,0,65535,0,0,0,65535,0,0,0,65535,0,0,0,65535,0,0,0,65535,0,0,0,65535,0,0,65535,65535,0,0,65535,65535,0,0,65535,65535,0,0,65535,65535,0,0,0,0,0,0,0,0,0]",
-                bigFont="false",
-                scrollText="auto",
-                centerText="false",
-                )
-
-if not currently_playing:
-    print("[DEBUG] No device playing.")
+#check if jellyfin is reachable / check for status 200
+try:
+    if requests.get(config.jellyfin['url']).status_code == 200:
+        #print("Returncode is 200")
+        devices = requests.get(
+            config.jellyfin['url'] +
+            '/Sessions?ActiveWithinSeconds=90',
+            headers=sendheaders)
+        getInfo(devices)
+except:
+    print("[ERROR] No connection to Jellyfin server under",config.jellyfin['url'])
     pixelit.skipApp()
+    
+
+
